fafa3090e7fa9270d2f04820c4cfe8367276cb18
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-07 05:17:40 +0000

    [Create]index,createアクション,ジャンル一覧ページとジャンル新規登録のルーティング

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,19 @@
 class Admin::GenresController < ApplicationController
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+  
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to '/genres'
+  end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+  
 end
