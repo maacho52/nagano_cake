7cdd6732766a7358904307d0230d1f04dd2a8010
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-08 08:19:11 +0000

    [Add]create,edit,updateアクション

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -3,17 +3,27 @@ class Admin::GenresController < ApplicationController
     @genre = Genre.new
     @genres = Genre.all
   end
-  
+
   def create
     @genre = Genre.new(genre_params)
     @genre.save
-    redirect_to '/genres'
+    redirect_to '/admin/genres'
   end
-  
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @ganre.update
+    redirect_to '/admin/genres'
+  end
+
   private
-  
+
   def genre_params
     params.require(:genre).permit(:name)
   end
-  
+
 end
