4c0a8c225f338ddff5d3ecb38def6261eed45dcc
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-08 08:22:39 +0000

    [Fix]controller#indexの前のadminを削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
-    get "genres" => "admin/genres#index"
-    post "genres" => "admin/genres#create"
+    get 'genres' => 'genres#index'
+    post 'genres' => 'genres#create'
+    get 'genres/:id/edit' => 'genres#edit'
+    patch 'genres/:id' => 'genres#update'
   end
 end
