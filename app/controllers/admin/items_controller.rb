9a9bb6cf2fae1a4b5315c2f53e2636a547112b7e
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-10 12:58:30 +0000

    [Create]new,index,show,item_paramsアクション

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,21 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+  
   def index
     @items = Item.all.page(params[:page]).per(10)
-    
   end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
+  end
+  
+  
 end
