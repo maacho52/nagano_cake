e4328226745e13faf5deae3cfd901b6bee19c845
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-12 10:46:15 +0000

    [Create]updateアクション

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,30 +2,36 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def index
     @items = Item.all.page(params[:page]).per(10)
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
-  
+
   def create
     @item = Item.new(item_params)
     @item.save
     redirect_to admin_items_path(@item.id)
   end
-  
+
   def edit
     @item = Item.find(params[:id])
-  end  
-  
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item)
+  end
+
   private
-  
+
   def item_params
     params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
   end
-  
-  
+
+
 end
