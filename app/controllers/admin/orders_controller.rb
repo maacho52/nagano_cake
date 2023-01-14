02256dcb58f2cb92f80bb1066209f517c4a543b4
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-14 14:12:12 +0000

    [Add]showアクション

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,4 +2,9 @@ class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.all.page(params[:page]).per(10)
   end
+
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+  end
 end
