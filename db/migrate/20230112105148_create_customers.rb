e12c2cbec67eef4a844921db4eae425f66b416ed
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-14 02:05:05 +0000

    [Fix]カラム名 emain=>email

--- db/migrate/20230112105148_create_customers.rb
+++ db/migrate/20230112105148_create_customers.rb
@@ -6,7 +6,7 @@ class CreateCustomers < ActiveRecord::Migration[6.1]
 
       t.string :last_name_kana, null: false
       t.string :first_name_kana, null: false
-      t.string :emain, null: false
+      t.string :email, null: false
       t.string :encrypted_password, null: false
       t.string :postal_code, null: false
       t.string :address, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2023_01_12_105148) do
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "emain", null: false
+    t.string "email", null: false
     t.string "encrypted_password", null: false
     t.string "postal_code", null: false
     t.string "address", null: false
