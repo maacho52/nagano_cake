75e784a35c991c99f8485c88bde07eb08aa83712
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-14 04:14:21 +0000

    [Create]index,full_name,customer_paramsアクション

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,16 @@
 class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all.page(params[:page]).per(10)
+  end
+
+    # 会員フルネーム
+  def full_name
+    self.last_name + " " + self.first_name
+  end
+
+  private
+
+  def customer_params
+    params.permit(:customer).require(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted )
+  end
 end
