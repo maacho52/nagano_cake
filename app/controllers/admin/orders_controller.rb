14c643d0d7d7828a74239f3baee75719f0feb2c6
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-15 02:01:24 +0000

    [Add]order_params

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,4 +7,10 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details
   end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:customer_id, :portal_code, :address, :name, :shipping_cost)
+  end
 end
