94702f0992a12b6c9681633002b55cb81a29f27c
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-15 02:02:12 +0000

    [Fix]customer_params requireとpermitを逆に

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,7 +2,7 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.all.page(params[:page]).per(10)
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
   end
@@ -11,6 +11,6 @@ class Admin::CustomersController < ApplicationController
   private
 
   def customer_params
-    params.permit(:customer).require(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted )
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted )
   end
 end
