b21c7f494916023b0f8e4b233ebc0b7d5331f7c9
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-18 14:27:31 +0000

    [Fix]URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
   namespace :public do
-    get 'orders/new'
+    #get 'cart_items/index'
   end
   namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
+    #get 'items/index'
+    #get 'items/show'
   end
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   scope module: :public do
     #get 'items' => 'items#index'(例)
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    #get 'orders/new'
+    resources :orders, only: [:new, :index, :show]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
   end
   namespace :admin do
    # get 'genres' => 'genres#index'
