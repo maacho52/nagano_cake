ef9a87e7c58c1187b481aca56f67796a10557e1e
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-21 08:07:46 +0000

    [Create]show,customer_paramsアクション

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,4 +2,15 @@ class Public::CustomersController < ApplicationController
   def new
     @customer = Customer.new
   end
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
+  end 
+  
 end
