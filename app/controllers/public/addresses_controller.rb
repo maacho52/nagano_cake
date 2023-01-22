0074eafdb8b4c0a26d8272340222ac7f0a94e223
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-22 00:09:13 +0000

    [Add]befre_action,indexアクションの@addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,5 +1,9 @@
 class Public::AddressesController < ApplicationController
+  before_action :authentic_customer!
+  
   def index
     @address = Address.new
+    @addresses = current_customer.addresses
+    
   end
 end
