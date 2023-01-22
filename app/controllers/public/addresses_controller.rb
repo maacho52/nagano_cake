af70a959e6e0c7ebd142b033451181e3faf899e9
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-22 00:27:59 +0000

    [Add]create,edit,address_paramsアクション

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,9 +1,23 @@
 class Public::AddressesController < ApplicationController
   before_action :authentic_customer!
-  
+
   def index
     @address = Address.new
     @addresses = current_customer.addresses
-    
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
   end
 end
