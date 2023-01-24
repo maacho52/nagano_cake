dd0edeb32671186adb145fd90bbc4da6e045e114
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-24 13:09:56 +0000

    [Create]indexアクション内のインスタンス変数

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   end
 end
