28bfafa5279baf28d179b7e424146b3c3860b843
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-28 13:47:23 +0000

    [Add]index,@cart_item createアクション

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,13 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = current_customer.cart_items.all
+    @cart_item = Cart_item.new
     @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+  end
+
+  def create
+    @cart_item = current_customer.cart_item.new(cart_item.params)
+    if current_customer.cart_item.present?
+      
   end
 end
