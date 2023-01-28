ef3b22857641ebccbdaea40155461f7d765674af
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-28 13:20:42 +0000

    [Add]check,create,order_paramsアクション

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,4 +3,19 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
     @customer = current_customer
   end
+  
+  def check
+    @cart_items = current_customer.cart_items.all
+    @order = Order.new(order_params)
+  end
+  
+  def create
+    @cart_item = Cart_item.find(params[:id])
+  end
+  
+  private 
+  
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :order_status)
+  end
 end
