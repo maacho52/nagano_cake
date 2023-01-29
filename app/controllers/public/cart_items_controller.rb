199ffad0c3127542e4d7074bda4f5b4684a47a24
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-29 10:40:43 +0000

    [Fix]Createアクション

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,8 +6,36 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = current_customer.cart_item.new(cart_item.params)
-    if current_customer.cart_item.present?
-      
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_item.save
+    redirect_to cart_items_path
+    
+    #if current_customer.cart_item.present?
+    #カートが存在する場合、そうでない場合
+
+  end
+
+  def update
+    @cart_item = Cart_item.find(params[:id])
+    if @cart_item.update
+       redirect_to cart_items_path
+       #数量など更新された場合
+    elsif
+      #カート内商品がすべてなくなった場合
+
+    end
+  end
+
+  def destroy
+
+  end
+
+  def destroy_all
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
   end
 end
