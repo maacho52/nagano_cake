25aa30c049d8ed420cbf33565f2f6a00d998d2a7
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-29 12:45:30 +0000

    [Delete]不要なルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   namespace :public do
     #get 'cart_items/index'
   end
-  devise_for :customers, controllers: {
+#skipオプションで不要なルーティングを削除
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
-devise_for :admin, controllers: {
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
