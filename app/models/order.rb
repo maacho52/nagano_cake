61cf7d56cc2f62deb28312209df3a3f201fd8b81
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-01-31 12:21:32 +0000

    [Fix]enum導入方法

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
-  enum payment_method: [ "クレジットカード", "銀行振込" ]
+  enum payment_method: { credit_card: 0, transfer: 1 }
   enum address: [ "ご自身の住所", "登録済住所から選択", "新しいお届け先" ]
 end
