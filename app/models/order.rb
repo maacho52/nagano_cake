0bc1461ddfdb1646bf1518251e928046ee1df765
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-02-02 13:23:11 +0000

    [Fix]注文ステータスenum管理

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
+  # 支払方法
   enum payment_method: { credit_card: 0, transfer: 1 }
-  enum address: [ "ご自身の住所", "登録済住所から選択", "新しいお届け先" ]
+  
+  # 注文ステータス（0=入金待ち / 1=入金確認 / 2=製作中 / 3=発送準備中 / 4=発送済み）
+  enum order_status: { waiting: 0, paid_up: 1, making: 2, preparing: 3, shipped: 4 }
 end
