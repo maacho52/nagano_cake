c78f24e90d4febcc85bab5b3752ad3e831032dcb
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-02-05 03:23:07 +0000

    [Add]enum 製造ステータス

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,10 @@ class Order < ApplicationRecord
   has_many :order_details
   # 支払方法
   enum payment_method: { credit_card: 0, transfer: 1 }
-  
+
   # 注文ステータス（0=入金待ち / 1=入金確認 / 2=製作中 / 3=発送準備中 / 4=発送済み）
   enum order_status: { waiting: 0, paid_up: 1, making: 2, preparing: 3, shipped: 4 }
+
+  # 製造ステータス（0=製作不可 / 1=製作待ち / 2=製作中 / 3=製作完了 ）
+  enum making_status: { impossible: 0, waiting: 1, making: 2, completed: 3 }
 end
