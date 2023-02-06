7c119a7d640963829ed0528726cc2d1bd94bfba3
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-02-06 13:26:51 +0000

    [Add]update destroyアクション

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -17,6 +17,18 @@ class Public::AddressesController < ApplicationController
     @address = Address.find(params[:id])
   end
 
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
+  end
+  
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+
   private
 
   def address_params
