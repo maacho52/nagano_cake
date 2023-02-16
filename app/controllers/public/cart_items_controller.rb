ed31071ceb15853859009114d32aa643782269ca
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-02-16 12:28:02 +0000

    [Fix]update,createアクション

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -14,11 +14,11 @@ class Public::CartItemsController < ApplicationController
     #@item = Item.find(cart_item_params[:item_id])
     @cart_item = current_customer.cart_items.new(cart_item_params)
 
-    if current_customer.cart_items.find_by(item_id: params[:item_id]).present?
-      cart_item = current_customer.cart_item.find_by(item_id: params[:cart_item][:item_id])
-      cart_item.amount += params[:cart_item][:amount].to_i
-      cart_item.save
-      redirect_to cart_items_path
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+       cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+       cart_item.amount += params[:cart_item][:amount].to_i
+       cart_item.save
+       redirect_to cart_items_path
     else
       @cart_item.save
       redirect_to cart_items_path
@@ -33,7 +33,7 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
-    @cart_item = Cart_item.find(params[:id])
+    @cart_item = CartItem.find(params[:id])
     if params[:cart_item][:amount] == "0"
       @cart_item.destroy　##カートに商品がない場合カート削除
       redirect_to cart_items_path
