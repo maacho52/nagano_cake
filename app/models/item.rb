f3fcbc6ce3b37428e91710990aa19cd4f490180b
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-02-18 02:38:51 +0000

    [Fix]validates スペルミス

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   validates :is_active, inclusion: [true, false]
   validates :name, presence: true
   validates :introduction, presence: true
-  validates :price, presence: true, numericality: { only_integer: ture } 
+  validates :price, presence: true, numericality: { only_integer: true } 
   ## 消費税を求めるメソッド
   def with_tax_price
     (price * 1.1).floor
