b51e62523e18706d9ab5e5bff4bc4f7faeda4d0c
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-02-18 10:34:46 +0000

    [Fix]スペルミス

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,7 +3,7 @@ class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
 
-  validates :amount, presence: ture, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
   ##小計を求めるメソッド
   def subtotal
