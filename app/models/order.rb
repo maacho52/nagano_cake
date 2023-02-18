7b4f53c4b79af7d9b42c478d852501ee2148c038
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-02-18 02:18:17 +0000

    [Delete]making_status enum[Add]validates

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,7 @@ class Order < ApplicationRecord
   # 注文ステータス（0=入金待ち / 1=入金確認 / 2=製作中 / 3=発送準備中 / 4=発送済み）
   enum order_status: { waiting: 0, paid_up: 1, making: 2, preparing: 3, shipped: 4 }
 
-  # 製造ステータス（0=製作不可 / 1=製作待ち / 2=製作中 / 3=製作完了 ）
-  enum making_status: { impossible: 0, production_waiting: 1, production_making: 2, completed: 3 }
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
 end
