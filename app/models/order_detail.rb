c9352424f3b0dc00dfabec3691913668caf9bef9
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-02-18 02:18:57 +0000

    [Add]enum makins_status

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,7 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
+  
+  # 製造ステータス（0=製作不可 / 1=製作待ち / 2=製作中 / 3=製作完了 ）
+  enum making_status: { impossible: 0, production_waiting: 1, production_making: 2, completed: 3 }
 end
