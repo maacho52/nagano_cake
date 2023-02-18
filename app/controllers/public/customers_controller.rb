b95d0c46356d370d431da3a2dd3302ee2807d237
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-02-18 03:03:53 +0000

    [Create]confrim,withdrawアクション

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -17,6 +17,16 @@ class Public::CustomersController < ApplicationController
     redirect_to customers_my_page_path
   end
 
+  def confirm
+    @customer = current_customer
+  end
+
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
   private
 
   def customer_params
