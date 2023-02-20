28b3322c42dc642ac8fa104e253267baf7bdf1b7
maacho52 <117494270+maacho52@users.noreply.github.com>
2023-02-20 13:32:25 +0000

    [Add]after_sign_up_path_fo

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -15,6 +15,9 @@ class Public::RegistrationsController < Devise::RegistrationsController
     super
    end
 
+  def after_sign_up_path_for(resource)
+    customers_my_page_path
+  end
   # GET /resource/edit
   # def edit
   #   super
